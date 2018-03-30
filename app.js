e6bd1bbccc4a02e04acec49af0e4581be84a7b6c
Saif <kotwal61969@gmail.com>
2018-03-31 02:41:55 +0530

    mLab fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes = require("./routes/comments"),
     indexRoutes = require("./routes/index");
 
 // mongoose.connect("mongodb://localhost/yelp_camp_v6");
-mongoose.connect("mongodb://saif:FarzanaSKN1997!@ds229909.mlab.com:29909/yelpcamp");
+mongoose.connect("mongodb://saif:farzana@ds229909.mlab.com:29909/yelpcamp");
 app.use(bodyParser.urlencoded({
     extended: true
 }));
