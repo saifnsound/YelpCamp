21e2ad19b0f0fc423631bcf22787617c661d70a5
Saif <kotwal61969@gmail.com>
2018-03-31 03:23:33 +0530

    listen fix

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 2000, function () {
     console.log("The YelpCamp Server Has Started!");
 });
